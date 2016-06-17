17e5be731ea5c4a983dc8e1cf76a198b81354e40
Jacob Pierson <Jacob Pierson>
2016-06-17 15:57:11 -0500

    added button that prints to console

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,7 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import Header from './Header';
 import PictureFrame from './PictureFrame';
+import Button from './Button';
 
 
 class Container extends React.Component {
@@ -13,11 +14,18 @@ class Container extends React.Component {
     	<div>
     	<Header text = "Playstation vs Xbox" />
     	<PictureFrame src1 = "../images/devil.jpg" src2="../images/cheif.jpg"/>
+    	<Button onClick = {this._printMessage}/>
     	</div>
-    
     );
   }
+
+  _printMessage() {
+	console.log('Hello');
+  }
+
 }
+
+
 
 Container.defaultProps = { };
 
